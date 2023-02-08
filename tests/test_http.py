220fe5556b1cf227b55ec9039acbbdefe8d19326
Roman Soiko <111957788r@gmail.com>
2023-02-08 20:21:49 +0200

    Tests status code body

---
+++ tests/test_http.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(f"Response is {r.text}")
+
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/vanpelt')
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Van Pelt'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/roman_soiko')
+
+    assert r.status_code == 400
