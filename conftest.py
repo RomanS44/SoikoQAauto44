b16b4082961832f34cb28b7fde058adbf39024aa
Roman Soiko <111957788r@gmail.com>
2023-02-08 18:14:57 +0200

    Updated to my name

--- conftest.py
+++ conftest.py
@@ -8,8 +8,8 @@ class User:
         self.second_name = None
 
     def create(self):
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
+        self.name = 'Roman'
+        self.second_name = 'Soiko'
 
     def remove(self):
         self.name = ''
--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -2,9 +2,9 @@ import pytest
 
 @pytest.mark.check
 def test_change_name(user):
-    assert user.name == 'Sergii'
+    assert user.name == 'Roman'
     
 
 @pytest.mark.check
 def test_change_second_name(user):
-    assert user.second_name == 'Butenko'
+    assert user.second_name == 'Soiko'
